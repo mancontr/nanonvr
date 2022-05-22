7fea89f3c975c283308fb217d7533b23a8e505b6
Anxo <mancontr@gmail.com>
2022-05-22 02:29:11 +0200

    Replace sqlite3 with better-sqlite3

--- src/server/services/db.ts
+++ src/server/services/db.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import sqlite3 from 'sqlite3'
+import SqliteDatabase from 'better-sqlite3'
 import { v4 as uuid } from 'uuid'
 import { Camera } from 'src/types'
 import { dataDir } from 'src/config'
@@ -7,74 +7,50 @@ import { dataDir } from 'src/config'
 const defaultPath = path.join(dataDir, 'data.db')
 
 export class Database {
-  db: sqlite3.Database = null
+  db: SqliteDatabase = null
 
   constructor(altPath: string = defaultPath) {
-    this.db = new sqlite3.Database(altPath)
+    this.db = new SqliteDatabase(altPath)
     this.initialize()
   }
 
   initialize() {
-    this.db.run(`CREATE TABLE IF NOT EXISTS cam (
+    this.db.prepare(`CREATE TABLE IF NOT EXISTS cam (
       uuid TEXT NOT NULL PRIMARY KEY,
       name TEXT NOT NULL,
       streamMain TEXT NOT NULL,
       streamSub TEXT,
       snapshot TEXT
-    )`)
+    )`).run()
   }
 
-  getCameras(): Promise<Camera[]> {
-    return new Promise((resolve, reject) => {
-      this.db.all('SELECT * FROM cam', (err, rows) => {
-        err ? reject(err) : resolve(rows)
-      })
-    })
+  getCameras(): Camera[] {
+    return this.db.prepare('SELECT * FROM cam').all()
   }
 
-  getCamera(id: string): Promise<Camera> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .prepare('SELECT * FROM cam WHERE uuid = ?')
-        .get([id], (err, row) => {
-          err ? reject(err) : resolve(row)
-        })
-    })
+  getCamera(id: string): Camera {
+    return this.db.prepare('SELECT * FROM cam WHERE uuid = ?').get([id])
   }
 
-  async addCamera(cam: Camera): Promise<Camera> {
+  addCamera(cam: Camera): Camera {
     const id = uuid()
-    await new Promise((resolve, reject) => {
-      this.db
-        .prepare(`INSERT INTO cam(uuid, name, streamMain, streamSub, snapshot) values (?, ?, ?, ?, ?)`)
-        .run(
-          [id, cam.name, cam.streamMain, cam.streamSub, cam.snapshot],
-          function (err) { err ? reject(err) : resolve(null)}
-        )
-    })
+    this.db
+      .prepare(`INSERT INTO cam(uuid, name, streamMain, streamSub, snapshot) values (?, ?, ?, ?, ?)`)
+      .run([id, cam.name, cam.streamMain, cam.streamSub, cam.snapshot])
     return { ...cam, uuid: id }
   }
 
-  async updateCamera(id: string, cam: Camera): Promise<Camera> {
-    await new Promise((resolve, reject) => {
-      this.db
-        .prepare(`UPDATE cam SET name = ?, streamMain = ?, streamSub = ?, snapshot = ? WHERE uuid = ?`)
-        .run(
-          [cam.name, cam.streamMain, cam.streamSub, cam.snapshot, id],
-          function (err) { err ? reject(err) : resolve(null)}
-        )
-    })
+  updateCamera(id: string, cam: Camera): Camera {
+    this.db
+      .prepare(`UPDATE cam SET name = ?, streamMain = ?, streamSub = ?, snapshot = ? WHERE uuid = ?`)
+      .run([cam.name, cam.streamMain, cam.streamSub, cam.snapshot, id])
     return { ...cam, uuid: id }
   }
 
-  async removeCamera(id: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .prepare('DELETE FROM cam WHERE uuid = ?')
-        .get([id], (err) => {
-          err ? reject(err) : resolve(null)
-        })
-    })
+  removeCamera(id: string): void {
+    this.db
+      .prepare('DELETE FROM cam WHERE uuid = ?')
+      .run([id])
   }
 
 }
