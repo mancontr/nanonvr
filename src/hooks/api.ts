7f77687ed1771abce931332504b800d0ba096f9a
Anxo <mancontr@gmail.com>
2022-07-11 09:33:15 +0200

    Add keys to useFetch calls

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,12 +2,12 @@ import { useCRUD, useFetch } from '@gluedigital/ruse-fetch-extras'
 import { Camera, Track } from 'src/types'
 import { baseUrl as base } from 'src/config'
 
-export const useCameras = () => useFetch<Camera[]>(base + '/cameras')
-export const useCamera = (id: string) => useFetch<Camera>(id && (base + '/cameras/' + id))
+export const useCameras = () => useFetch<Camera[]>(base + '/cameras', null, 'cams')
+export const useCamera = (id: string) => useFetch<Camera>(id && (base + '/cameras/' + id), null, 'cam-' + id)
 
 export const useCameraCRUD = () => {
   const crud = useCRUD<string, Camera>(base + '/cameras')
   return crud
 }
 
-export const useCameraTracks = (id: string) => useFetch<Track[]>(id && (base + '/cameras/' + id + '/tracks'))
+export const useCameraTracks = (id: string) => useFetch<Track[]>(id && (base + '/cameras/' + id + '/tracks'), null, 'tracks-' + id)
