3500653a40069e48d5e4db140813c2f7f7c16022
Anxo <mancontr@gmail.com>
2022-07-24 13:11:39 +0200

    Fix bug determining current track

--- src/hooks/playback.ts
+++ src/hooks/playback.ts
@@ -5,10 +5,9 @@ import { useCameraTracks } from "./api";
 
 export const useTrackFromPlayPoint = (playPoint: PlayPoint): Track => {
   const camTracks = useCameraTracks(playPoint?.camId)
-  const track = useMemo(() => {
-    return playPoint && camTracks
-      .map(trackAddDates)
-      .find(t => t.start <= playPoint.ts && t.start + t.length >= playPoint.ts)
-  }, [camTracks])
+  const tracksWithDates = useMemo(() => camTracks?.map(trackAddDates) || [], [camTracks])
+  const ts = playPoint?.ts
+  const track = ts && tracksWithDates
+    .find(t => t.start <= ts && t.start + t.length >= ts)
   return track
 }
