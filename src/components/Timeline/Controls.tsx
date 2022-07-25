57e100458a8e4362deaaf5f19659167e0301cb1c
Anxo <mancontr@gmail.com>
2022-07-25 15:17:20 +0200

    Replace icons with icomoon

--- src/components/Timeline/Controls.tsx
+++ src/components/Timeline/Controls.tsx
@@ -23,15 +23,15 @@ const Controls = ({ slice, setSlice, playPoint }: ControlsProps) => {
     <div id="controls">
       <div className="control-block" />
       <div className="control-block">
-        <div className="button play" />
-        <div className="button stop" />
-        <a className={'button download' + (url ? '' : ' disabled')} download href={url} />
+        <div className="button icon-play" />
+        <div className="button icon-stop" />
+        <a className={'button icon-download' + (url ? '' : ' disabled')} download href={url} />
       </div>
       <div className="control-block">
-        <div className="button left" onClick={prev} />
-        <div className="button right" onClick={next} />
-        <div className="button zoom-in" onClick={zoomIn} />
-        <div className="button zoom-out" onClick={zoomOut} />
+        <div className="button icon-rewind" onClick={prev} />
+        <div className="button icon-ff" onClick={next} />
+        <div className="button icon-zoom-in" onClick={zoomIn} />
+        <div className="button icon-zoom-out" onClick={zoomOut} />
       </div>
     </div>
   )
