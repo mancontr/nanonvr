0981460016a9e09367adadbd1b220bc530532777
Anxo <mancontr@gmail.com>
2022-12-26 21:30:53 +0100

    Timeline: update pan/zoom so it works with any pointer

--- src/components/Timeline/useTimelineEvents.ts
+++ src/components/Timeline/useTimelineEvents.ts
@@ -1,56 +1,52 @@
 import { useRef } from "react"
 import { useSliceState } from "./SliceContext"
 
-interface StartingState {
-  positions: number[]
+interface ActionState {
+  starts: Map<number, number>
+  lasts: Map<number, number>
   slice: [number, number]
 }
 
 const useTimelineEvents = () => {
   const [slice, setSlice] = useSliceState()
-  const starts = useRef<StartingState>({ positions: [], slice: slice })
+  const s = useRef<ActionState>({ starts: new Map(), lasts: new Map(), slice: slice })
   const element = useRef<HTMLDivElement>()
-
-  const getX = (t: any): number[] => {
-    const touches = Array.from<any>(t)
-    touches.sort((a, b) => a.screenX - b.screenX) // Sort L->R so zoom can always work
-    return touches.map(t => t.screenX)
-  }
 
   return {
     ref: element,
-    onTouchStart: (e) => {
-      starts.current = {
-        positions: getX(e.touches),
-        slice: slice,
-      }
+    onPointerDown: (e) => {
+      element.current.setPointerCapture(e.pointerId)
+      s.current.lasts.set(e.pointerId, e.screenX)
+      s.current.lasts.forEach((v, k) => s.current.starts.set(k, v))
+      s.current.slice = slice
     },
-    onTouchEnd: (e) => {
-      starts.current = {
-        positions: getX(e.touches),
-        slice: slice,
-      }
+    onPointerUp: (e) => {
+      s.current.starts.delete(e.pointerId)
+      s.current.lasts.delete(e.pointerId)
+      s.current.lasts.forEach((v, k) => s.current.starts.set(k, v))
+      s.current.slice = slice
     },
-    onTouchMove: (e) => {
-      const touches = getX(e.touches)
-      if (touches.length !== starts.current.positions.length) {
-        // Touches don't match; restablish...
-        starts.current = {
-          positions: getX(e.touches),
-          slice: slice,
-        }
-      }
-      const deltas = touches.map((t, i: number) => t - starts.current.positions[i])
+    onPointerMove: (e) => {
+      if (!s.current.starts.has(e.pointerId)) return // Just a hover
+      s.current.lasts.set(e.pointerId, e.screenX)
+
+      const ids = Array.from(s.current.lasts.keys())
       const totalWidth = element.current.clientWidth
-      const [t0, t1] = starts.current.slice
+      const [t0, t1] = s.current.slice
       const totalTime = t1 - t0
-      if (deltas.length === 1) {
-        const relTime = totalTime * deltas[0] / totalWidth
+      if (ids.length === 1) {
+        const delta = s.current.lasts.get(ids[0]) - s.current.starts.get(ids[0])
+        const relTime = totalTime * delta / totalWidth
         setSlice([t0 - relTime, t1 - relTime])
-      } else if (deltas.length === 2) {
-        const relTime1 = totalTime * deltas[0] / totalWidth
-        const relTime2 = totalTime * deltas[1] / totalWidth
-        setSlice([t0 - relTime1, t1 - relTime2])
+      } else if (ids.length === 2) {
+        const st0 = s.current.starts.get(ids[0])
+        const st1 = s.current.starts.get(ids[1])
+        const la0 = s.current.lasts.get(ids[0])
+        const la1 = s.current.lasts.get(ids[1])
+        const middleDelta = ((la0 + la1) - (st0 + st1)) / 2
+        const distDelta = (Math.abs(la0 - la1) - Math.abs(st0 - st1)) / 2
+        const coef = totalTime / totalWidth
+        setSlice([t0 - coef * (middleDelta - distDelta), t1 - coef * (middleDelta + distDelta)])
       }
     }
   }
