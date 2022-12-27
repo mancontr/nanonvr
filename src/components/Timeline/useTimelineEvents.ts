ccaf941004bcb28a484b3af5fbf56eb5767b2407
Anxo <mancontr@gmail.com>
2022-12-27 02:18:17 +0100

    Timeline: fix odd behaviour on drag from play block

--- src/components/Timeline/useTimelineEvents.ts
+++ src/components/Timeline/useTimelineEvents.ts
@@ -2,49 +2,60 @@ import { useRef } from "react"
 import { useSliceState } from "./SliceContext"
 
 interface ActionState {
-  starts: Map<number, number>
-  lasts: Map<number, number>
+  taps: Map<number, {
+    start: number
+    last: number
+    target?: any
+    ts: number
+  }>
   slice: [number, number]
 }
 
 const useTimelineEvents = () => {
   const [slice, setSlice] = useSliceState()
-  const s = useRef<ActionState>({ starts: new Map(), lasts: new Map(), slice: slice })
+  const s = useRef<ActionState>({ taps: new Map(), slice: slice })
   const element = useRef<HTMLDivElement>()
 
   return {
     ref: element,
     onPointerDown: (e) => {
-      (e.target || element.current).setPointerCapture(e.pointerId)
-      s.current.lasts.set(e.pointerId, e.screenX)
-      s.current.lasts.forEach((v, k) => s.current.starts.set(k, v))
+      element.current.setPointerCapture(e.pointerId)
+      s.current.taps.set(e.pointerId, {
+        start: e.screenX,
+        last: e.screenX,
+        target: e.target,
+        ts: Date.now()
+      })
+      s.current.taps.forEach((v) => v.start = v.last)
       s.current.slice = slice
     },
     onPointerUp: (e) => {
-      s.current.starts.delete(e.pointerId)
-      s.current.lasts.delete(e.pointerId)
-      s.current.lasts.forEach((v, k) => s.current.starts.set(k, v))
+      const tap = s.current.taps.get(e.pointerId)
+      s.current.taps.delete(e.pointerId)
+      s.current.taps.forEach((v) => v.start = v.last)
       s.current.slice = slice
+      if ((Date.now() - tap.ts) < 150) {
+        if (tap.target?.click) tap.target?.click()
+      }
     },
     onPointerMove: (e) => {
-      if (!s.current.starts.has(e.pointerId)) return // Just a hover
-      s.current.lasts.set(e.pointerId, e.screenX)
+      if (!s.current.taps.has(e.pointerId)) return // Just a hover
+      s.current.taps.get(e.pointerId).last = e.screenX
 
-      const ids = Array.from(s.current.lasts.keys())
+      const ids = Array.from(s.current.taps.keys())
       const totalWidth = element.current.clientWidth
       const [t0, t1] = s.current.slice
       const totalTime = t1 - t0
       if (ids.length === 1) {
-        const delta = s.current.lasts.get(ids[0]) - s.current.starts.get(ids[0])
+        const tap = s.current.taps.get(ids[0])
+        const delta = tap.last - tap.start
         const relTime = totalTime * delta / totalWidth
         setSlice([t0 - relTime, t1 - relTime])
       } else if (ids.length === 2) {
-        const st0 = s.current.starts.get(ids[0])
-        const st1 = s.current.starts.get(ids[1])
-        const la0 = s.current.lasts.get(ids[0])
-        const la1 = s.current.lasts.get(ids[1])
-        const middleDelta = ((la0 + la1) - (st0 + st1)) / 2
-        const distDelta = (Math.abs(la0 - la1) - Math.abs(st0 - st1)) / 2
+        const tap0 = s.current.taps.get(ids[0])
+        const tap1 = s.current.taps.get(ids[1])
+        const middleDelta = ((tap0.last + tap1.last) - (tap0.start + tap1.start)) / 2
+        const distDelta = (Math.abs(tap0.last - tap1.last) - Math.abs(tap0.start - tap1.start)) / 2
         const coef = totalTime / totalWidth
         setSlice([t0 - coef * (middleDelta - distDelta), t1 - coef * (middleDelta + distDelta)])
       }
