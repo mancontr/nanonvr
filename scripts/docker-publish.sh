f7dfa5b7b130c6ce6f9a897088166fa619fff568
Anxo <mancontr@gmail.com>
2024-03-24 12:32:17 +0100

    Update publish script to always tag according to package.json too

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mancontr/nanonvr:latest --push .
+TAG=$(cat package.json | grep version | cut -d\" -f4)
+sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mancontr/nanonvr:latest -t mancontr/nanonvr:${TAG} --push .
