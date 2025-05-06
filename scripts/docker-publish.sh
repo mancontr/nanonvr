65436250125e288d06c2e448a3b543f49be8bce2
Anxo <mancontr@gmail.com>
2025-05-06 09:52:33 +0200

    Deprecate armv7 The build was hanging forever and there are no users using it presently

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TAG=$(cat package.json | grep version | cut -d\" -f4)
-sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mancontr/nanonvr:latest -t mancontr/nanonvr:${TAG} --push .
+sudo docker buildx build --platform linux/amd64,linux/arm64 -t mancontr/nanonvr:latest -t mancontr/nanonvr:${TAG} --push .
