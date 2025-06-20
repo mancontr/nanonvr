cb00c68a77694b0129dcfe2ac6e4b29a94c74a4f
Anxo <mancontr@gmail.com>
2025-06-20 18:29:02 +0200

    update docker local test args to current settings

--- scripts/docker-local-test.sh
+++ scripts/docker-local-test.sh
@@ -4,4 +4,4 @@
 sudo docker buildx build --platform linux/amd64 -t mancontr/nanonvr:latest --load .
 
 # Run it
-docker run -it -v data:/share -v data:/data -p 8099:8099  mancontr/nanonvr:latest
+docker run -it -v $(pwd)/data:/media -v $(pwd)/data:/config -p 8099:8099 --name nanonvr-test --rm  mancontr/nanonvr:latest
